6a4838c5a0ac0859458c169cbff8269fc96f378d
yan-tar <17968850+yan-tar@users.noreply.github.com>
2024-07-26 23:00:11 +0200

    расширяем Item

--- tgbot2/fastapi/main.py
+++ tgbot2/fastapi/main.py
@@ -9,6 +9,10 @@ chunk = Chunk(path_to_base="Simble.txt")
 # класс с типами данных параметров 
 class Item(BaseModel): 
     text: str
+    summary: str
+
+class ShortItem(BaseModel): 
+    text: str
 
 # создаем объект приложения
 app = FastAPI()
@@ -29,18 +33,18 @@ def read_root():
 
 # функция обработки post запроса + декоратор 
 @app.post("/api/get_answer")
-def get_answer(question: Item):
+def get_answer(question: ShortItem):
     answer = chunk.get_answer(query=question.text)
     return {"message": answer}
 
 # асинхронная функция обработки post запроса + декоратор 
 @app.post("/api/get_answer_async")
 async def get_answer_async(question: Item):
-    answer = await chunk.async_get_answer(query=question.text)
+    answer = await chunk.async_get_answer(query=question.text, summary=question.summary)
     return {"message": answer}
 
 @app.post("/api/get_summary")
-async def get_summary(history:Item):    
+async def get_summary(history: ShortItem):    
     print("Мы ожидаем саммари, вот наша история: ", history.text)
     answer = await chunk.async_get_summary(history=history.text)
     return {"message": answer}
